f65e7d7df04d449aaca88c07f2ee11825910cbd5
XIAOkeji6 <you@example.com>
2018-05-15 20:24:38 +0800

    ff

--- js/map.js
+++ js/map.js
@@ -73,17 +73,62 @@ $(function(){
     var map = new BMap.Map("map1");          // 创建地图实例
     var point = new BMap.Point(mapData.map[1].longitude,mapData.map[1].latitude);  // 创建点坐标
     map.centerAndZoom(point,21);                 // 初始化地图，设置中心点坐标和地图级别
-    //海量点 全部显示：
+    //初始：
     function aa() {
+        //海量点 全部显示：：
         if (document.createElement('canvas').getContext) {  // 判断当前浏览器是否支持绘制海量点
             var points = [];  // 添加海量点数据
             for (var i = 0; i < mapData.map.length; i++) {
                 points.push(new BMap.Point(mapData.map[i].longitude, mapData.map[i].latitude));
-            }
-            var options = {
-                size: BMAP_POINT_SIZE_SMALL,
-                shape:3,
-                color: 'blue'
+                if(mapData.map[i].RSRP <= -110){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#ff0000'
+                    }
+                }else if(mapData.map[i].RSRP <= -105 && mapData.map[i].RSRP > -110){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#ff00ff'
+                    }
+                }else if(mapData.map[i].RSRP <= -100 && mapData.map[i].RSRP > -105){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#ffff00'
+                    }
+                }else if(mapData.map[i].RSRP <= -95 && mapData.map[i].RSRP > -100){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#00ffff'
+                    }
+                }else if(mapData.map[i].RSRP <= -85 && mapData.map[i].RSRP > -95){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#0101fe'
+                    }
+                }else if(mapData.map[i].RSRP <= -70 && mapData.map[i].RSRP > -85){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#30fe30'
+                    }
+                }else if(mapData.map[i].RSRP <= -50 && mapData.map[i].RSRP > -70){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#009600'
+                    }
+                }else if(mapData.map[i].RSRP > -50){
+                    var options = {
+                        size: BMAP_POINT_SIZE_SMALL,
+                        shape:3,
+                        color: '#009600'
+                    }
+                }
             }
             var pointCollection = new BMap.PointCollection(points, options);  // 初始化PointCollection
             pointCollection.addEventListener('click', function (e) {
@@ -95,9 +140,32 @@ $(function(){
         } else {
             alert('请在chrome、safari、IE8+以上浏览器查看本示例');
         }
+
+        //覆盖物：【扇形】
+        function mapMarker(lng,lat,name,rotate,icon){
+            var point = new BMap.Point(lng,lat);
+            if(icon == "red"){
+                var myIcon = new BMap.Icon("img/red.png",new BMap.Size(12,30),{});
+            }else if(icon == "yellow"){
+                var myIcon = new BMap.Icon("img/yellow.png",new BMap.Size(12,30),{});
+            }else if(icon == "green"){
+                var myIcon = new BMap.Icon("img/green.png",new BMap.Size(12,30),{});
+            }
+            var marker = new BMap.Marker(point,{icon:myIcon});  // 创建标注
+            map.addOverlay(marker);              // 将标注添加到地图中
+            marker.setRotation(rotate);  //设置标注旋转30度
+            marker.addEventListener("click",getAttr);  //添加点击事件
+            function getAttr(){
+                var p = marker.getPosition();  //获取marker的位置
+                alert('基站('+ p.lng + ',' + p.lat+')\n'+'小区1:\n'+'小区2:\n'+'小区3:');  // 监听点击事件
+            }
+        }
+        mapMarker(121.223504,31.264521,"小区1",0,"red");//可 循环调用来 打点
+        mapMarker(121.223504,31.264521,"小区2",240,'green');
+        mapMarker(121.223504,31.264521,"小区3",120,'yellow');
     }
     aa();
-    //鼠标指向表格 增加海量点（高亮）：
+    //鼠标点击表格 增加海量点（高亮）：
     var data2=[];
     $(".table1 tbody tr").on("click",function(){
         clearInterval(jishiqiMap); //关闭计时器
@@ -112,6 +180,8 @@ $(function(){
         data2.push(jin);
         data2.push(wei);
         bb();
+
+        line(jin, wei); //调用画线
     });
     //高亮 打点函数：
     function bb() {
@@ -132,7 +202,14 @@ $(function(){
         }
     }
 
-
+//覆盖物：【画线函数】
+    function line(startlng,startlat){
+        var polyline = new BMap.Polyline([
+            new BMap.Point(startlng,startlat),
+            new BMap.Point(121.223504, 31.264521)
+        ], {strokeColor:"red", strokeWeight:4, strokeOpacity:1});
+        map.addOverlay(polyline);
+    }
 //map回放：
     var jishiqiMap; //全局定义 计时器
     $(".huifangButton li").click(function(){
@@ -170,6 +247,7 @@ $(function(){
                 data2.push(jin);
                 data2.push(wei);
                 bb();
+                line(jin, wei); //调用画线
 
                 var scrollI =(j/5).toFixed(0);console.log(scrollI);
                 if(j>=mapData.map.length){
